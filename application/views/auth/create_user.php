b6661e37b1da6552b58c22a5365928d2d72dad42
yansa novan <yansanovan.yn31@gmail.com>
2020-04-01 11:35:00 +0700

    change create_user

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,37 +1,42 @@
-
 <li>
 	<a href="<?= base_url('admin');?>" style="margin-bottom: 20px">
-	 Back
+		Back
 	</a>
 </li>
-<p>Register a new membership</p>
-<?= $this->session->flashdata('msgbox');?>
-<?= form_open();?>
-<div>
-	<?= form_label('username', 'username');?>
-	<?= form_input(['type'=>'text', 'name' => 'username', 'value'=> set_value("username"), 'placeholder'=>'username']);?>
-	<?= form_error('username')?>
+<div class="col-md-6 col-md-offset-3" style="margin-top: 40px">
+    <div class="well bs-component">
+        <?= $this->session->flashdata('msgbox');?>
+        <h4 align="center" style="margin-bottom: 40px">Register new user</h4>
+        <fieldset>
+        	<?= form_open();?>
+	        	<div class="form-group <?= form_error('username') ? 'has-error': null ?>">
+	        		<?= form_label('username', 'username');?>
+	        		<?= form_input(['type'=>'text', 'name' => 'username', 'class'=> 'form-control', 'value'=> set_value("username"), 'placeholder'=>'username']);?>
+	        		<?= form_error('username')?>
+	        	</div>
+	        	<div class="form-group <?= form_error('email') ? 'has-error': null ?>">
+	        		<?= form_label('email', 'email');?>
+	        		<?= form_input(['type'=>'email', 'name' => 'email', 'class'=> 'form-control', 'value'=> set_value("email"), 'placeholder'=>'Email']);?>
+	        		<?= form_error('email')?>
+	        	</div>
+	        	<div class="form-group <?= form_error('role') ? 'has-error': null ?>">
+	        		<?= form_label('role', 'role');?>
+	        		<?= form_dropdown('role', $role, set_value('role'), 'class="form-control"');?>
+	        		<?= form_error('role');?>
+	        	</div>
+	        	<div class="form-group <?= form_error('password') ? 'has-error': null ?>">
+	        		<?= form_label('password', 'password');?>
+	        		<?= form_input(['type'=>'password', 'name' => 'password', 'class'=> 'form-control',  'autocomplete'=>'off', 'placeholder'=>'password']);?>
+	        		<?= form_error('password');?>
+	        	</div>
+	        	<div class="form-group <?= form_error('confirm_password') ? 'has-error': null ?>">
+	        		<?= form_label('confirm password', 'confirm password');?>
+	        		<?= form_input(['type'=>'password', 'name' => 'confirm_password', 'class'=> 'form-control', 'autocomplete'=>'off', 'placeholder'=>'confirm password']);?>
+	        		<?= form_error('confirm_password');?>
+	        	</div>
+	        	<button type="submit">Register</button>
+        	<?= form_close();?>
+        </fieldset>
+    </div>
 </div>
-<div>
-	<?= form_label('email', 'email');?>
-	<?= form_input(['type'=>'email', 'name' => 'email', 'value'=> set_value("email"), 'placeholder'=>'Email']);?>
-	<?= form_error('email')?>
-</div>
-<div>
-	<?= form_label('role', 'role');?>
-	<?= form_dropdown('role', $role, set_value('role'), 'class="form-control"');?>
-	<?= form_error('role');?>
-</div>
-<div>
-	<?= form_label('password', 'password');?>
-	<?= form_input(['type'=>'password', 'name' => 'password', 'autocomplete'=>'off', 'placeholder'=>'password']);?>
-	<?= form_error('password');?>
-</div>
-<div>
-	<?= form_label('confirm password', 'confirm password');?>
-	<?= form_input(['type'=>'password', 'name' => 'confirm_password', 'autocomplete'=>'off', 'placeholder'=>'confirm password']);?>
-	<?= form_error('confirm_password');?>
-</div>
-	<button type="submit">Register</button>
-<?= form_close();?>
-</div>
+
