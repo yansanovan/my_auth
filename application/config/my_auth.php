baea2f8d3270a2e72ab5dcce785366b0b1936b92
yansanovan <yansanovan.yn31@gmail.com>
2020-04-04 16:30:18 +0700

    fix config my_auth

--- application/config/my_auth.php
+++ application/config/my_auth.php
@@ -43,7 +43,7 @@ $config['enable_redirect_role'] = TRUE;
 | array bellow. example : admin is role and home is a controller
 |
 */ 
-$config['redirect_role'] = array('admin' => 'admin', 'user'=>'user');
+$config['redirect_role'] = array('admin' => 'admin');
 /*
 | -------------------------------------------------------------------------
 | forgotten password setup
